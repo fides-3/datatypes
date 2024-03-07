624d7fc2bfde22f3d516a782c12e1ecf95799e8a
ntoiti <fidesluckia36@gmail.com>
2024-03-07 23:04:27 +0300

    first commit

---
+++ dartassign.dart
@@ -0,0 +1,26 @@
+//number(integer) used to represent a whole number
+void main(){
+  int age =25;
+  print ('age:$age');
+}
+//example of double data type represents decimal numbers
+void main(){
+double weight =65.5;
+print('weight:&weight');
+}
+
+//string represents text data
+void name(){
+string name='fides lucky';
+ptint('name:$name');
+}
+//list ordered elements
+list <int> numbers=[1,2,3,4,5,];
+print('numbers:&numbers');
+
+//maps
+map<string,dynamic>person={
+  'name':'jane',
+  'age':'30'
+};
+print('person:&person');
